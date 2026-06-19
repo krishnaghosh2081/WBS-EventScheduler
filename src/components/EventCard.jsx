61fb3a458e2a86e67ece6d40ee494e55d2325737
Ron-sagiv <pedrozeeg@gmail.com>
2026-06-19 17:29:41 +0200

    Added styling to event cards

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,14 +1,29 @@
 import { Link } from 'react-router';
 
 const EventCard = ({ event }) => {
-    return (
-        <Link to={`/events/${event.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-            <div className="p-4 border rounded shadow">
-                <h2 className="text-xl font-semibold">{event.title}</h2>
-                <p>{event.date}</p>
-            </div>
-        </Link>
-    );
+  return (
+    <Link
+      to={`/events/${event.id}`}
+      style={{ textDecoration: 'none', color: 'inherit' }}
+    >
+      <div className="card bg-base-100 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title">{event.title}</h2>
+          <p>{event.location}</p>
+          <div className="card-actions justify-end">
+            <span>
+              {new Date(event.date).toLocaleDateString('en-GB', {
+                day: 'numeric',
+                month: 'long',
+                year: 'numeric',
+              })}
+            </span>
+            {/* <button className="btn btn-primary">Buy Now</button> */}
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default EventCard;
