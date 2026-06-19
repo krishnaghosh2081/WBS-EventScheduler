96bc03673aa95e98e4a8cc208c99a52f998194f7
Ron-sagiv <pedrozeeg@gmail.com>
2026-06-19 14:32:10 +0200

    Added fetch logic to EventDetails without styling

--- src/pages/EventDetails.jsx
+++ src/pages/EventDetails.jsx
@@ -1,13 +1,31 @@
-import  {useAuthenticationContext}  from '../context/AuthenticationContext';
-import { useParams } from "react-router";
+import { useAuthenticationContext } from '../context/AuthenticationContext';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 
 const EventDetails = () => {
-  const { token} = useAuthenticationContext();
+  // const { token } = useAuthenticationContext();
   const { id } = useParams();
+  const [event, setEvent] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/api/events/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setEvent(data);
+      })
+      .catch((err) => console.error(err));
+  }, [id]);
+  if (!event) {
+    return <p>Loading....</p>;
+  }
   return (
-    <div > Event details for ID: {id}
+    <div>
+      Event details for ID: {id}
+      <h1>{event.title}</h1>
+      <p>{event.date}</p>
+      <p>{event.description}</p>
+      <p>{event.location}</p>
     </div>
   );
 };
-
 export default EventDetails;
